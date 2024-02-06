9ea3defa761c2cdaf73185f39f9e25765dd73c88
Dat Ngo <119279710+DatNgo-dev@users.noreply.github.com>
2024-02-06 18:20:25 +0800

    Initialise file and created the UI

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Button title="Player 1" />
+      </View>
+      <View
+        style={{
+          borderRadius: "250",
+          height: 250,
+          width: 250,
+          backgroundColor: "blue",
+        }}
+      >
+        <View
+          style={{
+            display: "flex",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 100,
+              color: "white",
+            }}
+          >
+            0
+          </Text>
+        </View>
+      </View>
+      <View>
+        <Button title="Player 2" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 30,
+    backgroundColor: "#e54e4e",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
