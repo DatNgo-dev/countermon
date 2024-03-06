b38a4e774f22aecc35c1500ca7d290379f538aff
Dat Ngo <119279710+DatNgo-dev@users.noreply.github.com>
2024-03-06 21:50:24 +0800

    Add rotation and name label

--- App.js
+++ App.js
@@ -1,81 +1,92 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import React, { useState } from "react";
 import {
-  Button,
-  Pressable,
   TouchableOpacity,
   StyleSheet,
   Text,
   View,
   Dimensions,
 } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
 
 const { height, width } = Dimensions.get("window");
 
 export default function App() {
-  [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
+  const sv = useSharedValue(0);
+
+  const containerStyle = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${sv.value}deg` }],
+  }));
 
   const increment = () => {
     if (count === 10) return;
     setCount((count) => count + 1);
+    if (count >= 0) {
+      sv.value = withTiming(180, { duration: 400 });
+    }
   };
 
   const decrement = () => {
     if (count === -10) return;
     setCount((count) => count - 1);
+    if (count <= 0) {
+      sv.value = withTiming(360, { duration: 400 });
+    }
   };
+
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        <TouchableOpacity style={styles.bottom} onPress={increment}>
-          <Text style={styles.text}>+</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.bottom} onPress={decrement}>
-          <Text style={styles.text}>-</Text>
+      <View>
+        <TouchableOpacity
+          style={{
+            height: height,
+            width: width / 3,
+          }}
+          className="flex justify-end p-5"
+          onPress={() => {
+            decrement();
+          }}
+        >
+          <Text className="bg-blue-400 justify-self-end p-2">Player 1</Text>
         </TouchableOpacity>
       </View>
       <View
         style={{
-          borderRadius: "250",
           height: 250,
           width: 250,
-          backgroundColor: "blue",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <View
-          style={{
-            display: "flex",
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
+        <Animated.View style={[containerStyle]}>
           <Text
             style={{
-              fontSize: 100,
+              fontSize: 200,
               color: "white",
             }}
           >
             {Math.abs(count)}
           </Text>
-        </View>
+        </Animated.View>
       </View>
-      <View
-        style={{
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        <TouchableOpacity style={styles.bottom} onPress={decrement}>
-          <Text style={styles.text}>-</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.bottom} onPress={increment}>
-          <Text style={styles.text}>+</Text>
+      <View>
+        <TouchableOpacity
+          style={{
+            height: height,
+            width: width / 3,
+          }}
+          className="flex justify-start p-5"
+          onPress={() => {
+            increment();
+          }}
+        >
+          <Text className="bg-blue-400 justify-self-end p-2 rotate-180">
+            Player 2
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -85,28 +96,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     display: "flex",
+    flexDirection: "row",
     flex: 1,
-    paddingTop: 30,
     backgroundColor: "#e54e4e",
     justifyContent: "space-evenly",
     alignItems: "center",
-  },
-  top: {
-    height: height / 3,
-    width: width,
-    backgroundColor: "skyblue",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  bottom: {
-    height: height / 3,
-    width: width / 2,
-    backgroundColor: "skyblue",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 24,
-    color: "white",
   },
 });
